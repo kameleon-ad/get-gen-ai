2bdbf6c47657823bf261a8bd68af446feabffc02
kameleon-ad <alvindelos439@gmail.com>
2024-03-06 19:22:43 +0100

    Complete the migration

--- auth/app/__init__.py
+++ auth/app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from app.api import api_bp
 from app.extensions import (
     DB_MIGRATER,
+    JWT,
     SQL_DB,
 )
 from config import Base
@@ -11,12 +12,13 @@ from config import Base
 def create_app(config = None):
     if config is None:
         config = Base
-    
+
     app = Flask(__name__)
     app.config.from_object(config)
 
     SQL_DB.init_app(app)
     DB_MIGRATER.init_app(app, SQL_DB)
+    JWT.init_app(app)
 
     app.register_blueprint(api_bp, url_prefix='/api')
 
--- auth/runserver.sh
+++ auth/runserver.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 flask db init
-flask db migrate
+flask db migrate -m "migrate at the docker"
+flask db upgrade
 
 gunicorn -b "auth-server:8000" "app:create_app()"
